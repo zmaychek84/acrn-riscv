projectacrn/acrn-hypervisor

config-tools: change 'DISABLED' settings to 'ENABLED'
Some configurator settings are defined as 'feature DISABLED' style, which
could cause confusion to developers. This patch is to change them into
straightforward 'ENABLED' style.

Including:
  - MCE_ON_PSC_DISABLED -> MCE_ON_PSC_ENABLED
  - ENFORCE_TURNOFF_AC -> AC_ENABLED
  - ENFORCE_TURNOFF_GP -> GP_ENABLED

Tracked-On: #7661

Signed-off-by: Wu Zhou <wu.zhou@intel.com>

misc/config_tools/xforms/config_common.xsl
@@ -73,11 +73,11 @@
     </xsl:call-template>
 
     <xsl:call-template name="boolean-by-key">
-      <xsl:with-param name="key" select="'ENFORCE_TURNOFF_AC'" />
+      <xsl:with-param name="key" select="'AC_ENABLED'" />
     </xsl:call-template>
 
     <xsl:call-template name="boolean-by-key">
-      <xsl:with-param name="key" select="'ENFORCE_TURNOFF_GP'" />
+      <xsl:with-param name="key" select="'GP_ENABLED'" />
     </xsl:call-template>
 
     <xsl:call-template name="boolean-by-key">
@@ -124,8 +124,8 @@
     </xsl:call-template>
 
     <xsl:call-template name="boolean-by-key-value">
-      <xsl:with-param name="key" select="'MCE_ON_PSC_WORKAROUND_DISABLED'" />
-      <xsl:with-param name="value" select="MCE_ON_PSC_DISABLED" />
+      <xsl:with-param name="key" select="'MCE_ON_PSC_WORKAROUND_ENABLED'" />
+      <xsl:with-param name="value" select="MCE_ON_PSC_ENABLED" />
     </xsl:call-template>
 
     <xsl:call-template name="boolean-by-key-value">

zmaychek84/acrn-riscv

  - ENFORCE_TURNOFF_AC -> SPLIT_LOCK_DETECTION_ENABLED
  - ENFORCE_TURNOFF_GP -> UC_LOCK_DETECTION_ENABLED

misc/config_tools/xforms/config_common.xsl
@@ -73,11 +73,11 @@
     </xsl:call-template>
 
     <xsl:call-template name="boolean-by-key">
-      <xsl:with-param name="key" select="'ENFORCE_TURNOFF_AC'" />
+      <xsl:with-param name="key" select="'SPLIT_LOCK_DETECTION_ENABLED'" />
     </xsl:call-template>
 
     <xsl:call-template name="boolean-by-key">
-      <xsl:with-param name="key" select="'ENFORCE_TURNOFF_GP'" />
+      <xsl:with-param name="key" select="'UC_LOCK_DETECTION_ENABLED'" />
     </xsl:call-template>
 
     <xsl:call-template name="boolean-by-key">
@@ -124,8 +124,8 @@
     </xsl:call-template>
 
     <xsl:call-template name="boolean-by-key-value">
-      <xsl:with-param name="key" select="'MCE_ON_PSC_WORKAROUND_DISABLED'" />
-      <xsl:with-param name="value" select="MCE_ON_PSC_DISABLED" />
+      <xsl:with-param name="key" select="'MCE_ON_PSC_WORKAROUND_ENABLED'" />
+      <xsl:with-param name="value" select="MCE_ON_PSC_ENABLED" />
     </xsl:call-template>
 
     <xsl:call-template name="boolean-by-key-value">