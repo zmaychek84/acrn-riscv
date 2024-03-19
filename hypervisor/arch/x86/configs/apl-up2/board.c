binbinwu1/acrn-hypervisor

hv: config: add msix emulation support
The information needed to enable MSI-x emulation.
Only enable MSI-x emuation for the devices in msix_emul_devs array.
Currently, only EHL has the need to enable MSI-x emulation for TSN
devices.

Tracked-On: #4831
Signed-off-by: Binbin Wu <binbin.wu@intel.com>
Acked-by: Eddie Dong <eddie.dong@intel.com>

hypervisor/arch/x86/configs/apl-up2/board.c
@@ -48,3 +48,4 @@ const union pci_bdf plat_hidden_pdevs[MAX_HIDDEN_PDEVS_NUM] = {
 		.bits.f = 0x0,
 	},
 };
+const struct vmsix_on_msi_dev_info vmsix_on_msi_devs[MAX_VMSIX_ON_MSI_PDEVS_NUM];

hypervisor/arch/x86/configs/dnv-cb2/board.c
@@ -23,3 +23,4 @@ struct platform_clos_info platform_mba_clos_array[MAX_PLATFORM_CLOS_NUM];
 
 const struct cpu_state_table board_cpu_state_tbl;
 const union pci_bdf plat_hidden_pdevs[MAX_HIDDEN_PDEVS_NUM];
+const struct vmsix_on_msi_dev_info vmsix_on_msi_devs[MAX_VMSIX_ON_MSI_PDEVS_NUM];

zmaychek84/acrn-riscv

hypervisor/arch/x86/configs/apl-up2/board.c
@@ -48,3 +48,4 @@ const union pci_bdf plat_hidden_pdevs[MAX_HIDDEN_PDEVS_NUM] = {
 		.bits.f = 0x0,
 	},
 };
+const struct vmsix_on_msi_info vmsix_on_msi_devs[MAX_VMSIX_ON_MSI_PDEVS_NUM];

hypervisor/arch/x86/configs/dnv-cb2/board.c
@@ -23,3 +23,4 @@ struct platform_clos_info platform_mba_clos_array[MAX_PLATFORM_CLOS_NUM];
 
 const struct cpu_state_table board_cpu_state_tbl;
 const union pci_bdf plat_hidden_pdevs[MAX_HIDDEN_PDEVS_NUM];
+const struct vmsix_on_msi_info vmsix_on_msi_devs[MAX_VMSIX_ON_MSI_PDEVS_NUM];