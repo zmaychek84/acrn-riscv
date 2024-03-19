projectacrn/acrn-hypervisor

hv: change 'DISABLED' settings to 'ENABLED'
In order to improve DX, 'DISABLED' style configurator settings are
changed to 'ENABLED' style. The meaning of the MACROs are reversed,
so in the hv code we have to change '#ifndef' -> '#ifdef' or
'#ifdef' -> '#ifndef'.

Including:
  - MCE_ON_PSC_DISABLED -> MCE_ON_PSC_ENABLED
  - ENFORCE_TURNOFF_AC -> AC_ENABLED
  - ENFORCE_TURNOFF_GP -> GP_ENABLED

Tracked-On: #7661
Acked-by: Eddie Dong <eddie.dong@intel.com>
Signed-off-by: Wu Zhou <wu.zhou@intel.com>

hypervisor/arch/x86/cpu.c
@@ -110,7 +110,7 @@ uint64_t get_active_pcpu_bitmap(void)
 
 static void enable_ac_for_splitlock(void)
 {
-#ifndef CONFIG_ENFORCE_TURNOFF_AC
+#ifdef CONFIG_AC_ENABLED
 	uint64_t test_ctl;
 
 	if (has_core_cap(CORE_CAP_SPLIT_LOCK)) {
@@ -118,12 +118,12 @@ static void enable_ac_for_splitlock(void)
 		test_ctl |= MSR_TEST_CTL_AC_SPLITLOCK;
 		msr_write(MSR_TEST_CTL, test_ctl);
 	}
-#endif /*CONFIG_ENFORCE_TURNOFF_AC*/
+#endif /*CONFIG_AC_ENABLED*/
 }
 
 static void enable_gp_for_uclock(void)
 {
-#ifndef CONFIG_ENFORCE_TURNOFF_GP
+#ifdef CONFIG_GP_ENABLED
 	uint64_t test_ctl;
 
 	if (has_core_cap(CORE_CAP_UC_LOCK)) {
@@ -131,7 +131,7 @@ static void enable_gp_for_uclock(void)
 		test_ctl |= MSR_TEST_CTL_GP_UCLOCK;
 		msr_write(MSR_TEST_CTL, test_ctl);
 	}
-#endif /*CONFIG_ENFORCE_TURNOFF_GP*/
+#endif /*CONFIG_GP_ENABLED*/
 }
 
 void init_pcpu_pre(bool is_bsp)

zmaychek84/acrn-riscv

  - ENFORCE_TURNOFF_AC -> SPLIT_LOCK_DETECTION_ENABLED
  - ENFORCE_TURNOFF_GP -> UC_LOCK_DETECTION_ENABLED

hypervisor/arch/x86/cpu.c
@@ -110,7 +110,7 @@ uint64_t get_active_pcpu_bitmap(void)
 
 static void enable_ac_for_splitlock(void)
 {
-#ifndef CONFIG_ENFORCE_TURNOFF_AC
+#ifdef CONFIG_SPLIT_LOCK_DETECTION_ENABLED
 	uint64_t test_ctl;
 
 	if (has_core_cap(CORE_CAP_SPLIT_LOCK)) {
@@ -118,12 +118,12 @@ static void enable_ac_for_splitlock(void)
 		test_ctl |= MSR_TEST_CTL_AC_SPLITLOCK;
 		msr_write(MSR_TEST_CTL, test_ctl);
 	}
-#endif /*CONFIG_ENFORCE_TURNOFF_AC*/
+#endif /*CONFIG_SPLIT_LOCK_DETECTION_ENABLED*/
 }
 
 static void enable_gp_for_uclock(void)
 {
-#ifndef CONFIG_ENFORCE_TURNOFF_GP
+#ifdef CONFIG_UC_LOCK_DETECTION_ENABLED
 	uint64_t test_ctl;
 
 	if (has_core_cap(CORE_CAP_UC_LOCK)) {
@@ -131,7 +131,7 @@ static void enable_gp_for_uclock(void)
 		test_ctl |= MSR_TEST_CTL_GP_UCLOCK;
 		msr_write(MSR_TEST_CTL, test_ctl);
 	}
-#endif /*CONFIG_ENFORCE_TURNOFF_GP*/
+#endif /*CONFIG_UC_LOCK_DETECTION_ENABLED*/
 }
 
 void init_pcpu_pre(bool is_bsp)