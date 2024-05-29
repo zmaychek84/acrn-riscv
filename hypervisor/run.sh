b74d2f30144cda6527f05a2a72d287e2a2daba25
Haicheng Li <haicheng.li@intel.com>
2024-05-29 12:48:47 +0800

    riscv: update run.sh with duo-VM command added
    But the default one is still single SOS VM case.
    
    Signed-off-by: Haicheng Li <haicheng.li@intel.com>

--- hypervisor/run.sh
+++ hypervisor/run.sh
@@ -2,15 +2,5 @@
 
 set -x
 
-#qemu-system-x86_64 -smp 4 -kernel build/hypervisor/acrn.32.out -gdb tcp::1235 -S -serial pty -machine kernel-irqchip=split -cpu Denverton -m 1G,slots=3,maxmem=4G
-#qemu-system-riscv64 -smp 4 -kernel build/acrn.elf -gdb tcp::1235 -S -serial pty -M virt -m 1G,slots=3,maxmem=4G
-#qemu-system-riscv64 -kernel build/acrn.elf -gdb tcp::1235 -S -serial stdio -M virt -m 1G,slots=3,maxmem=4G -bios none
-#qemu-system-riscv64 -smp 4 -kernel build/acrn.elf -gdb tcp::1235 -S -serial stdio -M virt -m 4G,slots=3,maxmem=8G -bios none
-#qemu-system-riscv64 -smp 4 -bios build/acrn.elf -gdb tcp::1235 -S -serial stdio -M virt -m 4G,slots=3,maxmem=8G -kernel build/acrn.elf
-#qemu-system-riscv64 -smp 4 -bios build/acrn.elf -gdb tcp::1235 -S -M virt -m 4G,slots=3,maxmem=8G -kernel ./Image -nographic
-#qemu-system-riscv64 -smp 4 -bios build/acrn.elf -gdb tcp::1235 -S -M virt -m 4G,slots=3,maxmem=8G -kernel ./Image -nographic
-#qemu-system-riscv64 -smp 4 -bios build/acrn.elf -M virt -m 4G,slots=3,maxmem=8G -kernel ./Image -nographic
-#qemu-system-riscv64 -smp 4 -bios build/acrn.elf -gdb tcp::1235 -S -serial stdio -M virt -m 4G,slots=3,maxmem=8G -kernel ./hc.elf
-qemu-system-riscv64 -smp 4 -bios build/acrn.elf -gdb tcp::1235 -S -M virt -m 4G,slots=3,maxmem=8G -kernel ./vmlinux -initrd ./initrd -nographic
-#qemu-system-riscv64 -smp 4 -bios build/acrn.elf -gdb tcp::1235 -S -serial stdio -M virt -m 4G,slots=3,maxmem=8G -kernel ./vmlinux -initrd ./initrd -nographic
-#qemu-system-riscv64 -smp 4 -bios build/acrn.elf -gdb tcp::1235 -S -serial stdio -M virt -m 4G,slots=3,maxmem=8G -kernel ./Image
+#qemu-system-riscv64 -smp 5 -bios build/acrn.elf -gdb tcp::1235 -S -M virt -m 4G,slots=3,maxmem=8G -kernel ./vmlinux.sos -initrd ./initrd -device loader,file=./Image.uos,addr=0xC1000000 -device loader,file=./initrd,addr=0xC9000000 -nographic
+qemu-system-riscv64 -smp 5 -bios build/acrn.elf -gdb tcp::1235 -S -M virt -m 4G,slots=3,maxmem=8G -kernel ./vmlinux -initrd ./initrd -nographic
