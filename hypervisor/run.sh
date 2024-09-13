4c7c1350ab1de3010b730b8c44b30630fa2d5bf0
Haicheng Li <haicheng.li@intel.com>
2024-09-13 21:40:36 +0800

    riscv: update run.sh to load duo_VMs
    Signed-off-by: Haicheng Li <haicheng.li@intel.com>

--- hypervisor/run.sh
+++ hypervisor/run.sh
@@ -9,5 +9,5 @@ if [[ x"${QEMU}" = x"" ]]; then
     QEMU=${DEFAULT_QEMU}
 fi
 
-#${QEMU} -smp 5 -bios build/acrn.elf -gdb tcp::1235 -S -M virt -m 4G,slots=3,maxmem=8G -kernel ./vmlinux.sos -initrd ./initrd -device loader,file=./Image.uos,addr=0xC1000000 -device loader,file=./initrd,addr=0xC9000000 -nographic
-${QEMU} -smp 5 -bios build/acrn.elf -gdb tcp::1235 -S -M virt -m 4G,slots=3,maxmem=8G -kernel ./vmlinux -initrd ./initrd -nographic
+${QEMU} -smp 5 -bios build/acrn.elf -gdb tcp::1235 -S -M virt -m 4G,slots=3,maxmem=8G -kernel ./vmlinux.sos -initrd ./initrd -device loader,file=./Image.uos,addr=0xC1000000 -device loader,file=./initrd,addr=0xC9000000 -nographic
+#${QEMU} -smp 5 -bios build/acrn.elf -gdb tcp::1235 -S -M virt -m 4G,slots=3,maxmem=8G -kernel ./vmlinux -initrd ./initrd -nographic
